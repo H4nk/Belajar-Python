d614c7badd28c6c500be70f825b3ddce6b81b640
Harry Setya Hadi <xmoensen@gmail.com>
2020-10-04 18:46:20 +0700

    Pertemuan Kedua
    Source Code Pada Pertemuan kedua

---
+++ pertemuan ke 2/Home.py
@@ -0,0 +1,16 @@
+print("Selamat Datang")
+print("Nama : Saya Harry Setya Hadi")
+
+#print("Channel H4nk Techno")
+#Ini adalah komentar
+#komentar Biasa
+""" Ini adalah komentar multiline
+Ini komentar line 1
+ini komentar line 2
+ini komentar line 3
+"""
+print("Belajar Python")
+a= 10
+print(a)
+a= 20
+print(a)
