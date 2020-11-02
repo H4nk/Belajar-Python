c6bbe5936a60470ad333bf425223e97a4fadb65a
Harry Setya Hadi <xmoensen@gmail.com>
2020-11-02 17:59:09 +0700

    Pertemuan Ke 10
    Pertemuan Ke 10

---
+++ Pertemuan Ke 10/input.py
@@ -0,0 +1,20 @@
+#inputan Python 3.6
+#nama = input("Masukan nama Lengkap :")
+#umur = input("Inputkan Umur : ")
+#umursaya = umur
+
+#print("Nama Lengkap saya adalah : " + nama)
+#print("Umur saya adalah : "+ umur)
+#inputan python 2.7
+#nama = raw_input("Masukan nama Lengkap :")
+#print("Nama Lengkap saya adalah :" + nama)
+lebar = input("Masukan Nilai Lebar :")
+panjang = input("masukan Nilai Panjang :")
+L = int(lebar)
+P = int(panjang)
+luas = L * P
+keliling = (2 * L) + ( 2 * P)
+print("Lebar = " + lebar)
+print("Panjang = "+ panjang)
+print("Luas adalah = "+ str(luas))
+print("Keliling adalah = "+str(keliling))
